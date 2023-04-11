aaf42681db58c67d413a33c0ce5cfe7675fa094d
Thanapoom <6230228121@student.chula.ac.th>
2023-04-11 15:57:54 +0700

    [ADD] add mock data integration link

--- frontend/components/Product.tsx
+++ frontend/components/Product.tsx
@@ -1,23 +1,26 @@
 import { Grid, Stack, Typography } from "@mui/material"
 
 export interface ProductProps{
-    name: string,
-    price : number
+    title: string,
+    price : number,
+    imageSrc: string,
+    id : number
 }
 
 export default function Product(props: ProductProps){
     return (
         <Grid item xs={6} md={3}>
+            <a href={"/products/" + props.id}>
             <Stack direction={'column'}>
-            <img src='https://thestandard.co/wp-content/uploads/2021/01/5-45.jpg'
-                style={{ maxWidth: "100%" }}
+            <img src={props.imageSrc}
+                style={{ maxWidth: "100%", maxHeight: "100%"}}
                 />
             <Stack direction={'row'} justifyContent={'space-between'} marginTop={2}>
-                <Typography color={'white'}>ซอยจุ๊</Typography>
-                <Typography color={'white'}>11 BTC</Typography>
+                <Typography color={'white'} maxWidth={'150px'}>{props.title}</Typography>
+                <Typography color={'white'}>{props.price} BTC</Typography>
             </Stack>
             </Stack>
-            
+            </a>
         </Grid>
     )
 
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,6 +2,8 @@ import Product from '@/components/Product'
 import DarkWebAppBar from '@/components/darkWebAppBar'
 import Logo from '@/components/logo'
 import {Container, Grid} from '@mui/material'
+import mock_data from '../../data/mock_data.json'
+import { useRouter } from 'next/router'
 
 export default function Home() {
   return (
@@ -9,12 +11,19 @@ export default function Home() {
     <DarkWebAppBar pageName='หน้าหลัก'></DarkWebAppBar>
     <Logo></Logo>
     <Container>
-    <Grid container spacing={10} marginTop={'8px'}>
-      <Product name={'asdf'} price={0}></Product>
-      <Product name={'asdf'} price={0}></Product>
-      <Product name={'asdf'} price={0}></Product>
-      <Product name={'asdf'} price={0}></Product>
-      <Product name={'asdf'} price={0}></Product>
+    <Grid container spacing={5} paddingTop={5} paddingBottom={10}>
+      {
+        mock_data.map((element)=>{
+          return (
+            <Product title={element.Title} 
+            price={element.Price} 
+            imageSrc={element.Img}
+            key={element.ID}
+            id={element.ID}
+            ></Product>
+          )
+        })
+      }
     </Grid>
     </Container>
   </>
