62cad2e2ed9e0f8b1e362a1a4ab22c3bf9c60a17
Thanapoom <6230228121@student.chula.ac.th>
2023-04-26 11:12:46 +0700

    [FIX] CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,11 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  })
   await app.listen(8080);
   Logger.log("Running on port 8080", "NestApplication")
 }
