5e301c76ca07c4d3a6c44c264529c9ef6064bf27
Thanapoom <6230228121@student.chula.ac.th>
2023-04-29 12:17:53 +0700

    [ADD] toast after adding to cart

--- frontend/components/products/detail/ProductDetailContainer.tsx
+++ frontend/components/products/detail/ProductDetailContainer.tsx
@@ -3,6 +3,7 @@ import ProductDetailForm from "./ProductDetailForm";
 import { IProduct } from "@/models/Product";
 import { useDispatch } from "react-redux";
 import { addToCart } from "@/store/CartSlice";
+import { toast } from "react-hot-toast";
 
 interface Props {
   product: IProduct;
@@ -12,6 +13,7 @@ const ProductDetailContainer = ({ product }: Props) => {
   const dispatch = useDispatch();
   const handleBuyProduct = () => {
     dispatch(addToCart({_id : product._id, title : product.title, amount : 1, price: product.price}))
+    toast.success("เพิ่มไปยังรถเข็นแล้ว")
   };
   return (
     <ProductDetailForm product={product} handleBuyProduct={handleBuyProduct} />
