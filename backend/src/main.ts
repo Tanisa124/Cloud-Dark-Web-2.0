22b3bdf0b83a9a6c9ae812f4d31688b39ab826a3
Thanapoom <6230228121@student.chula.ac.th>
2023-05-02 17:59:05 +0700

    [FIX] allow CORS for all

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,8 +5,7 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: true,
     credentials: true,
   })
   await app.listen(8080);
